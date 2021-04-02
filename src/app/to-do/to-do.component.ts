b2d4719fe557d919176556e219e45a0f0f7e459a
Dan Cappellano <danielcapp99@gmail.com>
2021-04-01 20:53:44 -0400

    Lab1 commit

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from './../interfaces/to-do'
 
 @Component({
   selector: 'app-to-do',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent implements OnInit {
+
+  todos: ToDo[] = [
+    {task: 'take out garbage', completed: false},
+    {task: 'complete the lab', completed: false},
+    {task: 'pay a bill', completed: true},
+    {task: 'do laundry', completed: false}
+  ]
 
   constructor() { }
 
